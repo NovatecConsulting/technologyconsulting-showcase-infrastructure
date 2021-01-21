49a8b52638018d1015d66915b075ae6aa0fb2c48
Thorsten Jakoby <thorsten.jakoby@novatec-gmbh.de>
2021-01-21 11:23:07 +0100

    prepare aks

---
+++ modules/kubernetes.tf
@@ -0,0 +1,69 @@
+### SPI 
+
+resource "azuread_application" "aksApp" {
+  display_name               = "aks-tc-showcase-${var.environment}"
+  available_to_other_tenants = false
+  oauth2_allow_implicit_flow = true
+}
+
+resource "azuread_service_principal" "aksSpi" {
+  application_id = azuread_application.aksApp.application_id
+}
+
+resource "random_string" "aksSpiPasswordGen" {
+  length  = 128
+  upper   = true
+  lower   = true
+  number  = true
+  special = true
+}
+
+resource "azuread_application_password" "aksSpiSecret" {
+  application_object_id = azuread_application.aksApp.id
+  value                 = random_string.aksSpiPasswordGen.result
+  description           = "aks-tc-showcase-${var.environment}"
+  end_date_relative     = "8760h"
+}
+
+### ACR PULL
+
+resource "azurerm_role_assignment" "allowAksToPullFromAcr" {
+  scope                            = data.azurerm_container_registry.containerregistry.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azuread_service_principal.aksSpi.object_id
+  skip_service_principal_aad_check = true
+}
+
+
+
+# resource "azurerm_kubernetes_cluster" "k8s" {
+#     name                = "k8s-${azurerm_resource_group.resourceGroup.name}"
+#     location            = azurerm_resource_group.resourceGroup.location
+#     resource_group_name = azurerm_resource_group.resourceGroup.name
+#     dns_prefix          = "k8s-${azurerm_resource_group.resourceGroup.name}"
+#     kubernetes_version  = "1.18.14"
+
+
+#     default_node_pool {
+#         name       = "default"
+#         node_count = 1
+#         vm_size    = "Standard_D2_v2"
+#     }
+
+#     addon_profile {
+#         oms_agent {
+#         enabled                    = true
+#         log_analytics_workspace_id = azurerm_log_analytics_workspace.k8sanalyticsworkspace.id
+#         }
+#     }
+
+#     service_principal {
+#         client_id     = azuread_application.aksApp.application_id
+#         client_secret = random_string.aksSpiPasswordGen.result
+#     }
+
+#     lifecycle {
+# #        prevent_destroy = true
+#         ignore_changes = ["default_node_pool"]
+#     }
+# }
--- modules/secretstorage.tf
+++ modules/secretstorage.tf
@@ -1,15 +1,15 @@
 resource "azurerm_key_vault" "vault" {
   name                        = "vault-${azurerm_resource_group.resourceGroup.name}"
-  location                    = "${azurerm_resource_group.resourceGroup.location}"
-  resource_group_name         = "${azurerm_resource_group.resourceGroup.name}"
+  location                    = azurerm_resource_group.resourceGroup.location
+  resource_group_name         = azurerm_resource_group.resourceGroup.name
   enabled_for_disk_encryption = true
-  tenant_id                   = "${data.azurerm_client_config.current.tenant_id}"
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
 
   sku_name = "standard"
 
   access_policy {
-    tenant_id = "${data.azurerm_client_config.current.tenant_id}"
-    object_id = "${var.developergroup}"
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = var.developergroup
 
     secret_permissions = [
       "get", "list", "set", "delete", "recover", "backup", "restore"
@@ -17,8 +17,8 @@ resource "azurerm_key_vault" "vault" {
   }
 
   access_policy {
-    tenant_id = "${data.azurerm_client_config.current.tenant_id}"
-    object_id = "${var.spigroup}"
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = var.spigroup
 
     secret_permissions = [
       "get", "list", "set", "delete"
@@ -27,8 +27,8 @@ resource "azurerm_key_vault" "vault" {
 
   # also need to grant the Service Principal being used with Terraform access to the KeyVault
   access_policy {
-    tenant_id = "${data.azurerm_client_config.current.tenant_id}" 
-    object_id = "${data.azurerm_client_config.current.object_id}"
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
 
     secret_permissions = [
       "get", "list", "set", "delete"
