8004c8c4c4c5354a4bc7ba2bb2ac251b0203252b
Immanuel Haag <immae1@users.noreply.github.com>
2021-02-01 11:06:26 +0100

    Update publicips.tf
    new syntax

--- modules/publicips.tf
+++ modules/publicips.tf
@@ -1,8 +1,8 @@
 # ---- orderdomain ip for lbl service type
 resource "azurerm_public_ip" "publicip" {
   name                = "orderdomain-publicip-${var.environment}"
-  location            = "${var.location}"
-  resource_group_name = "${var.environment}"
+  location            = azurerm_resource_group.resourceGroup.location
+  resource_group_name = azurerm_resource_group.resourceGroup.name
   allocation_method   = "Static"
   domain_name_label   = "orderdomain-${var.environment}"
   sku                 = "Standard"
@@ -10,12 +10,12 @@ resource "azurerm_public_ip" "publicip" {
 
 resource "azurerm_key_vault_secret" "publicip" {
   name          = "orderdomain-publicip"
-  value         = "${azurerm_public_ip.publicip.ip_address}"
-  key_vault_id  = "${data.azurerm_key_vault.vault.id}"
+  value         = azurerm_public_ip.publicip.ip_address
+  key_vault_id  = data.azurerm_key_vault.vault.id
 }
 
 resource "azurerm_key_vault_secret" "fqdn" {
   name          = "orderdomain-azurefqdn"
-  value         = "${azurerm_public_ip.publicip.fqdn}"
-  key_vault_id  = "${data.azurerm_key_vault.vault.id}"
+  value         = azurerm_public_ip.publicip.fqdn
+  key_vault_id  = data.azurerm_key_vault.vault.id
 }
