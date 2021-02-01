ce1ead3ab77c19a56bac54c823ba9f8785ab9b98
Immanuel Haag <immae1@users.noreply.github.com>
2021-02-01 10:55:12 +0100

    Create publicips.tf
    for orderdomian

---
+++ modules/publicips.tf
@@ -0,0 +1,21 @@
+# ---- orderdomain ip for lbl service type
+resource "azurerm_public_ip" "publicip" {
+  name                = "orderdomain-publicip-${var.environment}"
+  location            = "${var.location}"
+  resource_group_name = "${var.environment}"
+  allocation_method   = "Static"
+  domain_name_label   = "orderdomain-${var.environment}"
+  sku                 = "Standard"
+}
+
+resource "azurerm_key_vault_secret" "publicip" {
+  name          = "orderdomain-publicip"
+  value         = "${azurerm_public_ip.publicip.ip_address}"
+  key_vault_id  = "${data.azurerm_key_vault.vault.id}"
+}
+
+resource "azurerm_key_vault_secret" "fqdn" {
+  name          = "orderdomain-azurefqdn"
+  value         = "${azurerm_public_ip.publicip.fqdn}"
+  key_vault_id  = "${data.azurerm_key_vault.vault.id}"
+}
