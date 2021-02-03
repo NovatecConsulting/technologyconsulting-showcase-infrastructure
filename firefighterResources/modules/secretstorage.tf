5996957ddd3b5e8c4e18117cfcaa7fc9ba8da3fc
jakobyte1024 <thorsten.jakoby@novatec-gmbh.de>
2021-02-03 15:55:10 +0000

    data lookup

--- firefighterResources/modules/secretstorage.tf
+++ firefighterResources/modules/secretstorage.tf
@@ -1,4 +1,4 @@
 data "azurerm_key_vault" "vault" {
-  name                        = "vault-${azurerm_resource_group.resourceGroup.name}"
-  resource_group_name         = azurerm_resource_group.resourceGroup.name
+  name                  = "vault-${data.azurerm_resource_group.resourceGroup.name}"
+  resource_group_name   = data.azurerm_resource_group.resourceGroup.name
 }
