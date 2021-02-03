9938200922806e6638b305aa588c6dd54f4256bb
iha <immanuel.haag@novatec-gmbh.de>
2021-02-03 14:28:01 +0100

    first we want one pubipforingresscontroller

--- showcaseInfrastructure/modules/publicips.tf
+++ showcaseInfrastructure/modules/publicips.tf
@@ -1,33 +1,3 @@
-# INGRESS
-resource "azurerm_public_ip" "kubernetes_cluster_ingress_ip" {
-
-  for_each = toset(local.stages)
-  name                         = "${each.key}-${var.environment}-ingress-ip"
-  resource_group_name          = azurerm_resource_group.resourceGroup.name
-  location                     = azurerm_resource_group.resourceGroup.location
-  allocation_method            = "Static"
-  sku                          = "Standard"
-  domain_name_label            = "${each.key}-ingress-${var.environment}"
-}
-
-resource "azurerm_key_vault_secret" "ingress-ip" {
-  name         = "${each.key}-ingress-ip"
-  value        = azurerm_public_ip.kubernetes_cluster_ingress_ip[each.key].ip_address
-  key_vault_id = azurerm_key_vault.vault.id
-}
-
-resource "azurerm_key_vault_secret" "ingress-fqdn" {
-  name         = "${each.key}-ingress-fqdn"
-  value        = azurerm_public_ip.kubernetes_cluster_ingress_ip[each.key].fqdn
-  key_vault_id = azurerm_key_vault.vault.id
-}
-
-resource "azurerm_role_assignment" "allowAksSpiToContributeIngressIp" {
-  scope                = azurerm_public_ip.kubernetes_cluster_ingress_ip[each.key].id
-  role_definition_name = "Contributor"
-  principal_id         = azuread_service_principal.aksSpi.object_id
-}
-
 # ---- orderdomain ip for lbl service type
 resource "azurerm_public_ip" "publiciporder" {
   for_each = toset(local.stages)  
