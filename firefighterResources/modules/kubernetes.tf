b0444ed70ca16b9a96e4d112091360398f794205
jakobyte1024 <thorsten.jakoby@novatec-gmbh.de>
2021-02-04 09:24:02 +0000

    datatatatata

--- firefighterResources/modules/kubernetes.tf
+++ firefighterResources/modules/kubernetes.tf
@@ -1,5 +1,5 @@
 data "azurerm_kubernetes_cluster" "k8s" {
-  name                = "k8s-${azurerm_resource_group.resourceGroup.name}"
+  name                = "k8s-${data.azurerm_resource_group.resourceGroup.name}"
   resource_group_name = data.azurerm_resource_group.resourceGroup.name
 }
 
