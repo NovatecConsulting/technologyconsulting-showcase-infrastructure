2ffb9c14014f27f9f7d76a54c6e8813cb0934d8e
Patrick <patrick.eichhorn@novatec-gmbh.de>
2021-05-07 14:18:15 +0200

    changed local repo path

--- showcaseInfrastructure/modules/sock-shop.tf
+++ showcaseInfrastructure/modules/sock-shop.tf
@@ -7,7 +7,7 @@ resource "kubernetes_namespace" "sock-shop" {
 resource "helm_release" "sock-shop-helm-chart" {
   depends_on = [kubernetes_namespace.sock-shop]
   name       = "sock-shop-helm-chart"
-  repository    = "../../sock-shop-helm/helm-chart"
+  repository    = "sock-shop-helm/helm-chart"
   chart      = "sock-shop"
   namespace  = "sock-shop" 
   timeout    = "500"
