9c841d953039cac15679e0bfebcdb3ad766f2c55
Patrick <patrick.eichhorn@novatec-gmbh.de>
2021-05-07 15:21:15 +0200

    fix tf

--- showcaseInfrastructure/modules/sock-shop.tf
+++ showcaseInfrastructure/modules/sock-shop.tf
@@ -12,7 +12,7 @@ resource "null_resource" "debuggerv2" {
 
 
 resource "helm_release" "sock-shop-helm-chart" {
-  depends_on = [kubernetes_namespace.sock-shop, null_resource.debugger]
+  depends_on = [kubernetes_namespace.sock-shop]
   name       = "sock-shop-helm-chart"
   chart      = "sock-shop-helm/helm-chart"
   namespace  = "sock-shop" 
