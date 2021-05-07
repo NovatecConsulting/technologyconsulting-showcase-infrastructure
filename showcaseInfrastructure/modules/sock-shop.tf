a4ecd6b65c57bec109603c811be2cc997e5ae1cc
Patrick <patrick.eichhorn@novatec-gmbh.de>
2021-05-07 13:50:51 +0200

    added sock-shop helm deployment

--- showcaseInfrastructure/modules/sock-shop.tf
+++ showcaseInfrastructure/modules/sock-shop.tf
@@ -19,7 +19,7 @@ resource "kubernetes_namespace" "sock-shop" {
 resource "helm_release" "sock-shop-helm-chart" {
   depends_on = [kubernetes_namespace.sock-shop]
   name       = "sock-shop-helm-chart"
-  chart      = "sock-shop-helm/helm-chart/Chart.yaml"
+  chart      = "sock-shop-helm/helm-chart"
   namespace  = "sock-shop" 
   timeout    = "500"
 }
